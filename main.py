98d1b4c5a2027f62d7cbdfb64dd9efe99789b536
unikhex <potatoc377@gmail.com>
2022-11-24 12:45:50 +0100

    created a folder for all the images being used

--- main.py
+++ main.py
@@ -45,17 +45,17 @@ def openTaskFile():
         file=open('tasklist.txt','w')
         file.close()
 #icon
-Image_icon=PhotoImage(file="task.png")
+Image_icon=PhotoImage(file="Images/task.png")
 root.iconphoto(False,Image_icon)
 
 #top bar
-TopImage = PhotoImage(file="topbar.png")
+TopImage = PhotoImage(file="Images/topbar.png")
 Label(root,image= TopImage).pack()
 
-dockImage=PhotoImage(file="dock.png")
+dockImage=PhotoImage(file="Images/dock.png")
 Label(root,image=dockImage,bg="#32405b").place(x=30,y=25)
 
-noteImage=PhotoImage(file="task.png")
+noteImage=PhotoImage(file="Images/task.png")
 Label(root,image=noteImage,bg="#32405b").place(x=30,y=25)
 
 heading=Label(root,text="ALL TASK",font="arial 20 bold",fg="white",bg="#32405b")
@@ -89,7 +89,7 @@ scrollbar.config(command=listbox.yview)
 openTaskFile()
 
 #delete
-Delete_icon=PhotoImage(file="delete.png")
+Delete_icon=PhotoImage(file="Images/delete.png")
 Button(root,image=Delete_icon,bd=0,command=deleteTask).pack(side=BOTTOM,pady=13)
 
 
